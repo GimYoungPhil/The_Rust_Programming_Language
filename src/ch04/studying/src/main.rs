7fc76626ef2e92b4eb2db265715087a3ce9d051d
YoungPhilGim <youngphil.gim@gmail.com>
2023-02-23 16:54:55 +0900

    fixed

--- src/ch04/studying/src/main.rs
+++ src/ch04/studying/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    
+    let mut s1 = String::from("안녕하세요"); // String
+
+    // s1.push_str(", world");
+
+    println!("{}", s1);
+    println!("{}", s1.len());
+    // println!("{}", s1);
 }
