d06e40a3fb86a51165a88fbe958fb428ae060ca2
YoungPhilGim <youngphil.gim@gmail.com>
2023-05-12 16:54:59 +0900

    fixed

--- src/ch00/hello/src/main.rs
+++ src/ch00/hello/src/main.rs
@@ -1,8 +1,10 @@
 fn main() -> () {
 
-    let mut x = 5;
+    let tup: (u32, f32, u32) = (1, 0.5, 3);
+    let rt: &(u32, f32, u32) = &tup;
 
-    x *= 2;
+    println!("{:?}", tup);
+    println!("{:?}", rt);
+    println!("end...");
 
-    println!("{}", x);
 }
---
+++ src/ch00/hello/src/main_00_char.rs
@@ -0,0 +1,17 @@
+fn main() -> () {
+
+    let a: char = 'a';
+    let b: char = 'b';
+    let c: char = 'c';
+
+    let d: char = '러';
+    let e: char = '스';
+    let f: char = '트';
+
+    println!("{a}");
+    println!("{b}");
+    println!("{c}");
+    println!("{d}");
+    println!("{e}");
+    println!("{f}");
+}
